d57692d449200799377902dbad13495d4d26821a
CordiiT14 <cordelia.toennies@live.com>
2022-03-30 14:11:56 +0100

    list all films and find film by title - completed

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,19 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.allFilmTitles = function() {
+    const result = this.films.map((film) =>  {
+      return film.title;
+    });
+    return result;
+};
+
+Cinema.prototype.findByTitle = function(title){
+  const result = this.films.filter((film) => {
+      return film.title === title;
+  });
+  return result;
+}
+
 module.exports = Cinema;
+
